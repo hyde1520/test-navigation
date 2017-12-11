5af610435bfb1cbc05af39b3d1255915e7b63fd2
hyde1520 <jhyde@uidaho.edu>
2017-12-11 12:42:30 -0800

    add a test help  folder (sandbox)

---
+++ HelpSandbox/Data/HelpSystem.js
@@ -0,0 +1,8 @@
+var xmlHelpSystemData = "";
+xmlHelpSystemData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlHelpSystemData += '<WebHelpSystem DefaultUrl=\"content/home.htm\" Toc=\"Data/Tocs/IFTDSS_Online_Output.js\" Index=\"Data/Index.js\" Concepts=\"Data/Concepts.js\" Glossary=\"Data/Glossary.js\" SearchDatabase=\"Data/Search.js\" Alias=\"Data/Alias.xml\" Synonyms=\"Data/Synonyms.xml\" SearchUrl=\"content/search.htm\" PathToScriptsFolder=\"Resources/Scripts/\" SkinName=\"HTML5 - Top Navigation\" SkinID=\"HTML5___Top_Navigation\" Multilingual=\"false\" Skins=\"HTML5 - Top Navigation\" BuildTime=\"12/11/2017 12:35:58 PM\" BuildVersion=\"13.3.6547.24746\" TargetType=\"WebHelp2\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" TopNavTocPath=\"false\" MoveOutputContentToRoot=\"false\" ReplaceReservedCharacters=\"false\" MakeFileLowerCase=\"true\" UseCustomTopicFileExtension=\"false\" PreventExternalUrls=\"false\" EnableResponsiveOutput=\"true\" IncludeGlossarySearchResults=\"true\" ResultsPerPage=\"10\" xml:lang=\"en-us\" LanguageName=\"English\" SearchEngine=\"MadCapSearch\">';
+xmlHelpSystemData += '    <CatapultSkin Version=\"2\" SkinType=\"WebHelp2\" ComponentType=\"MasterPage\" TemplateName=\"Fluid\" Comment=\"HTML5 Fluid Layout\" EnableResponsiveOutput=\"true\" RemoveImagesOnSave=\"true\" Name=\"HTML5 - Top Navigation\" SkinID=\"HTML5___Top_Navigation\" SkinClass=\"_Skins_HTML5___Top_Navigation\" HideNavOnStartup=\"False\" LogoUrl=\"\">';
+xmlHelpSystemData += '        <WebHelpOptions NavigationPanePosition=\"Right\" TopMenuDepth=\"3\" />';
+xmlHelpSystemData += '    </CatapultSkin>';
+xmlHelpSystemData += '</WebHelpSystem>';
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add('HelpSystem', xmlHelpSystemData);
---
+++ HelpSandbox/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=636485925587965214'
+});
